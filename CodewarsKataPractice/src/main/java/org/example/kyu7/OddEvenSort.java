4380f9f2fa900ae1e996128a7c2bb595dd27ef30
MarcHartleib <marc.hartleib91@gmail.com>
2023-03-28 10:25:14 +0200

    kyu7 Odd-Even String Sort  kata + test

---
+++ CodewarsKataPractice/src/main/java/org/example/kyu7/OddEvenSort.java
@@ -0,0 +1,8 @@
+package org.example.kyu7;
+
+public class OddEvenSort {
+
+    public String sortString(String sequence) {
+        return sequence;
+    }
+}
---
+++ CodewarsKataPractice/src/test/java/org/example/kyu7/OddEvenSortTest.java
@@ -0,0 +1,29 @@
+package org.example.kyu7;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OddEvenSortTest {
+
+    OddEvenSort oddEvenSort = new OddEvenSort();
+
+    @ParameterizedTest
+    @CsvSource({
+            "", "",
+            " ", " ",
+            "a", "a",
+            "ab", "a b",
+            "Wolfeschlegelsteinhausenbergerdorff", "Wleclgltihuebredrf ofsheesenasnegrof",
+            "METHIONYLTHREONYLTHREONYGLUTAMINYLARGINYL", "MTINLHENLHENGUAIYAGNL EHOYTROYTROYLTMNLRIY",
+            "PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS", "PEMNUTAIRSOISLCVLAOOISS NUOOLRMCOCPCIIOOCNCNOI",
+            "PSEUDOPSEUDOHYPOPARATHYROIDISM", "FOCNUIIIIIIIAIN LCIACNHLPLFCTO",
+            "SUBDERMATOGLYPHIC", "SBEMTGYHC UDRAOLPI",
+    })
+    void sortString(String input, String expected) {
+        String result = oddEvenSort.sortString(input);
+        assertEquals(expected, result);
+    }
+}
