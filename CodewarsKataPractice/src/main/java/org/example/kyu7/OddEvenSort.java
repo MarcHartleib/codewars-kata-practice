e0da4351ba41f0d5856f4764c6dfa3175aa1f08c
MarcHartleib <marc.hartleib91@gmail.com>
2023-03-28 11:26:51 +0200

    kyu7 Odd-Even String Sort kata solution +adjusted test

--- CodewarsKataPractice/src/main/java/org/example/kyu7/OddEvenSort.java
+++ CodewarsKataPractice/src/main/java/org/example/kyu7/OddEvenSort.java
@@ -1,8 +1,23 @@
 package org.example.kyu7;
 
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class OddEvenSort {
 
     public String sortString(String sequence) {
-        return sequence;
+        String evenString = IntStream.range(0, sequence.length())
+                .filter(c -> c % 2 == 0)
+                .mapToObj(sequence::charAt)
+                .map(Objects::toString)
+                .collect(Collectors.joining());
+        String oddString = IntStream.range(0, sequence.length())
+                .filter(c -> c % 2 != 0)
+                .mapToObj(sequence::charAt)
+                .map(Objects::toString)
+                .collect(Collectors.joining());
+
+        return oddString.length() == 0 ? evenString : evenString + " " + oddString;
     }
 }
--- CodewarsKataPractice/src/test/java/org/example/kyu7/OddEvenSortTest.java
+++ CodewarsKataPractice/src/test/java/org/example/kyu7/OddEvenSortTest.java
@@ -12,15 +12,14 @@ class OddEvenSortTest {
 
     @ParameterizedTest
     @CsvSource({
-            "", "",
-            " ", " ",
-            "a", "a",
-            "ab", "a b",
-            "Wolfeschlegelsteinhausenbergerdorff", "Wleclgltihuebredrf ofsheesenasnegrof",
-            "METHIONYLTHREONYLTHREONYGLUTAMINYLARGINYL", "MTINLHENLHENGUAIYAGNL EHOYTROYTROYLTMNLRIY",
-            "PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS", "PEMNUTAIRSOISLCVLAOOISS NUOOLRMCOCPCIIOOCNCNOI",
-            "PSEUDOPSEUDOHYPOPARATHYROIDISM", "FOCNUIIIIIIIAIN LCIACNHLPLFCTO",
-            "SUBDERMATOGLYPHIC", "SBEMTGYHC UDRAOLPI",
+            "a, a ",
+            "ab, a b",
+            "Wolfeschlegelsteinhausenbergerdorff, Wleclgltihuebredrf ofsheesenasnegrof",
+            "METHIONYLTHREONYLTHREONYGLUTAMINYLARGINYL, MTINLHENLHENGUAIYAGNL EHOYTROYTROYLTMNLRIY",
+            "PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS, PEMNUTAIRSOISLCVLAOOISS NUOOLRMCOCPCIIOOCNCNOI",
+            "PSEUDOPSEUDOHYPOPARATHYROIDISM, PEDPEDHPPRTYODS SUOSUOYOAAHRIIM",
+            "FLOCCINAUCINIHILIPILIFICATION, FOCNUIIIIIIIAIN LCIACNHLPLFCTO",
+            "SUBDERMATOGLYPHIC, SBEMTGYHC UDRAOLPI",
     })
     void sortString(String input, String expected) {
         String result = oddEvenSort.sortString(input);
