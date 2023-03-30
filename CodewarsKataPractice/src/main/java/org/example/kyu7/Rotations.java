d9e569d4eb0fcb141890edb72c5e70343292a3e8
MarcHartleib <marc.hartleib91@gmail.com>
2023-03-30 10:39:32 +0200

    daily kata rotations solved

--- CodewarsKataPractice/src/main/java/org/example/kyu7/Rotations.java
+++ CodewarsKataPractice/src/main/java/org/example/kyu7/Rotations.java
@@ -1,12 +1,19 @@
 package org.example.kyu7;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Rotations {
 
     //Codewars Link: https://www.codewars.com/kata/5700c9acc1555755be00027e/
 
     public boolean containsAllRotations(String str, List<String> arr) {
-        return false;
+        if (str.isEmpty()) {
+            return true;
+        }
+        return IntStream.range(0, str.length())
+                .mapToObj(i -> str.substring(i) + str.substring(0, i))
+                .filter(rotation -> rotation.length() == str.length())
+                .allMatch(arr::contains);
     }
 }
